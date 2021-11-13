9fe7e14d7bb86499a1b8f5fff56ec2c2cd0ae96a
meyoseph <meyoseph@gmail.com>
2021-11-13 16:08:11 -0600

    adds login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,22 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { ProfileCreateComponent } from './profiles/profile-create/profile-create.component';;
 import { HeaderComponent } from './header/header.component';
 import { ProfileListComponent } from './profiles/profile-list/profile-list.component';
 import { AppRoutingModule } from './app-routing.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProfileCreateComponent,
-    ProfileListComponent
+    ProfileListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +35,7 @@ import { AppRoutingModule } from './app-routing.component';
     MatButtonModule,
     MatToolbarModule,
     MatTableModule,
+    MatProgressSpinnerModule,
     HttpClientModule,
     AppRoutingModule
   ],
