733e0d5ef625c5ef69c5dfbfe28a2f0e8719a460
meyoseph <meyoseph@gmail.com>
2021-11-14 16:08:37 -0600

    adds auth interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
@@ -20,6 +20,7 @@ import { AppRoutingModule } from './app-routing.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,7 +5,12 @@ import { AuthData } from "./auth-data.model";
 import { LoginData } from "./login-data.model";
 @Injectable({ providedIn: 'root' })
 export class AuthService{
+  private tokenData: string = '';
   constructor(private http: HttpClient){
+  }
+
+  getToken(){
+    return this.tokenData;
   }
 
   createUser(email: string, password: string, phonenumber: string, userType: string){
@@ -17,8 +22,9 @@ export class AuthService{
 
   login(email: string, password: string){
     const loginData: LoginData = { email: email, password: password }
-    this.http.post('http://localhost:3000/api/users/login', loginData).subscribe((result) => {
-      console.log(result)
+    this.http.post<{ token: string }>('http://localhost:3000/api/users/login', loginData).subscribe((response) => {
+      const token = response.token;
+      this.tokenData = token;
     })
   }
 }
