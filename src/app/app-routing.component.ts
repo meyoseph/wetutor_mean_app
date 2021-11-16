e3c020679b5701edb9d45d52b99cb5a0094049f7
meyoseph <meyoseph@gmail.com>
2021-11-16 17:45:13 -0600

    adds guard to routes, redirect to parent when unknown routes used

--- src/app/app-routing.component.ts
+++ src/app/app-routing.component.ts
@@ -11,16 +11,17 @@ import { ListTutorsComponent } from "./tutors/list-tutors/list-tutors.component"
 import { ViewTutorsComponent } from "./tutors/view-tutors/view-tutors.component";
 
 const routes: Routes = [
-  {path: '', component: ListTutorsComponent, canActivate: [AuthGuard]},
+  {path: '', redirectTo: 'parent'},
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent},
   { path: 'profile', component: ProfileCreateComponent, canActivate: [AuthGuard] },
-  {path: 'view/:id', component: ViewTutorsComponent},
-  { path: 'admin', component: UsersListComponent},
-  {path:'admin/view/:id',component:UserDetailComponent},
+  {path: 'view/:id', component: ViewTutorsComponent, canActivate: [AuthGuard]},
+  { path: 'admin', component: UsersListComponent, canActivate: [AuthGuard]},
+  {path:'admin/view/:id',component:UserDetailComponent, canActivate: [AuthGuard]},
   { path: 'profile/detail/:id', component: ProfileDetailComponent, canActivate: [AuthGuard]},
   {path: 'parent', component: ListTutorsComponent, canActivate: [AuthGuard]} ,
-  {path: 'view/:id/:firstname/:lastname/:gender/:age/:educationlevel/:mainsubject/:language', component: ViewTutorsComponent, canActivate: [AuthGuard]}
+  {path: 'view/:id/:firstname/:lastname/:gender/:age/:educationlevel/:mainsubject/:language', component: ViewTutorsComponent, canActivate: [AuthGuard]},
+  { path: '**', redirectTo: 'parent'}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
